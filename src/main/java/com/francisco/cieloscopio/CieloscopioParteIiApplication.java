fdc8dd9bd2ade987511aa4383fe32bbce6cec156
Francisco-Thielen <fran.thielen@ispcloudservices.com>
2024-07-30 18:02:07 -0600

    Commit inicial

---
+++ src/main/java/com/francisco/cieloscopio/CieloscopioParteIiApplication.java
@@ -0,0 +1,13 @@
+package com.francisco.cieloscopio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CieloscopioParteIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CieloscopioParteIiApplication.class, args);
+	}
+
+}
